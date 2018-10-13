48ba123ad222b326fa9e86562c923462c73bfd08
sonoranora <nbiteniece@gmail.com>
2018-10-13 08:17:00 +0300

    inital

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {
+ app.get('/', function(req, res) {
+   res.render('pages/index');
+ });
+ app.get('/about', function(req, res) {
+   res.render('pages/about');
+ });
+};
